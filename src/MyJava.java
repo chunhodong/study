ae2f0373e32b3e1049eaa56aeeba6f44a43b46d1
chunhodong <yhms4432@naver.com>
2020-10-05 15:58:30 +0900

    first

---
+++ src/MyJava.java
@@ -0,0 +1,64 @@
+/*
+* 빌더 패턴
+*  - 복잡한 객체를 생성하는 방법과 표현하는 방법을 정의하는 클래스를 별도로 분리하여
+*  - 서로 다른 표현이라도 이를 생성할 수 있는 동일한 구축 공정을 제공할 수 있도록 한다.
+*  - 객체의 상태가 변하지 않고, 생성자에 비해서 코드가 간결해지고 직관성이 좋아짐
+*  - 다만 추가적인 빌더클래스를 구현해야하기떄문에 상황에 따라 코드량이 늘어날수있음
+*  - 인스턴스 변수가 많을경우 유리
+
+* public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // Required parameters(필수 인자)
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters - initialized to default values(선택적 인자는 기본값으로 초기화)
+        private int calories      = 0;
+        private int fat           = 0;
+        private int carbohydrate  = 0;
+        private int sodium        = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings    = servings;
+        }
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;    // 이렇게 하면 . 으로 체인을 이어갈 수 있다.
+        }
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+        public Builder carbohydrate(int val) {
+            carbohydrate = val;
+            return this;
+        }
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        servingSize  = builder.servingSize;
+        servings     = builder.servings;
+        calories     = builder.calories;
+        fat          = builder.fat;
+        sodium       = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+} */
+public class MyJava {
+}
